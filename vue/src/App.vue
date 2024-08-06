2ce8a08632a8ab7383e5ad962ad25f8ad7e9c819
liupeng0531 <133619837+liupeng0531@users.noreply.github.com>
2024-08-06 14:59:47 +0800

    前后端分离

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -50,7 +50,6 @@
   .silent {
     -webkit-filter: grayscale(100%);
     -moz-filter: grayscale(100%);
-    -ms-filter: grayscale(100%);
     -o-filter: grayscale(100%);
     filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
     _filter:none;
